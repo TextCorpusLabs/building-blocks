1f58c04272ffd88764ffe116278d45734818461f
Mark Newman <mark@trinetteandmark.com>
2020-11-13 11:25:15 -0500

    new agreator

--- code/corpus-to-json.py
+++ code/corpus-to-json.py
@@ -1,60 +1,91 @@
 import pathlib
+import jsonlines as jl
+import multiprocessing as mp
 import progressbar as pb
-import utils as u
+import typing as t
 from argparse import ArgumentParser
 from typeguard import typechecked
 
-# max lines per block
-_max_lines = 1500000
+_sentinel = None
 
 @typechecked
-def corpus_to_json(folder_in: pathlib.Path, json_out: pathlib.Path) -> None:
+def corpus_to_json(folder_in: pathlib.Path, jsonl_out: pathlib.Path) -> None:
     """
-    Iterates over all the documents in a corpus, aggregating them ~1.5 million lines at a time
+    Iterates over all the documents in a folder, aggregating them into a single json file
 
     Parameters
     ----------
     folder_in : pathlib.Path
         Folder containing the source corpus
-    json_out : pathlib.Path
+    jsonl_out : pathlib.Path
         JSON containing the aggregated corpus
     """
 
-    if json_out.exists():
-        json_out.unlink()
+    if jsonl_out.exists():
+        jsonl_out.unlink()
 
-    bar_i = 1
-    curr_block = 0
-    curr_lines = 0
-    widgets = [ 'Aggregating Document # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
-    with pb.ProgressBar(widgets = widgets) as bar:
-        for file_name in path_in.iterdir():
-            if u.is_corpus_document(file_name):
-                bar.update(bar_i)
-                bar_i = bar_i + 1
-                lines = __add_document_to_block(file_name, path_out, curr_block)
-                curr_lines = curr_lines + lines
-                if curr_lines >= _max_lines:
-                    curr_block = curr_block + 1
-                    curr_lines = 0
+    tasks = mp.JoinableQueue()
+    results = mp.JoinableQueue()
+
+    workers = _start_workers(tasks, results)
+    _fill_tasks(folder_in, tasks)
+    _save_results(results, jsonl_out)
+
+    i = 1
 
 @typechecked
-def __add_document_to_block(file_in_name: pathlib.Path, path_out: pathlib.Path, block: int) -> int:
-    with open(file_in_name, 'r', encoding = 'utf-8') as file_stream:
-        lines = file_stream.readlines()
-    
-    mode = 'w'
-    header = ['', f'--------- {file_in_name.name} ---------', '\n\n']
-    file_out_name = path_out.joinpath(f'./block.{block:05}.txt')
-    if file_out_name.exists():
-        mode = 'a'
-        header[0] = '\n'
-    
-    with open(file_out_name, mode, encoding = 'utf-8') as file_stream:
-        file_stream.writelines(header)
-        file_stream.writelines(lines)    
+def _start_workers(tasks: mp.Queue, results: mp.Queue) -> t.List[mp.Process]:
+    def _overlord(workers: t.List[mp.Process], results: mp.Queue) -> None:
+        for worker in workers:
+            worker.join()
+        results.put(_sentinel)
+        results.close()
+    workers = []
+    for _ in range(mp.cpu_count()):
+        worker = mp.Process(target = _process_tasks, args = (tasks, results))
+        worker.start()
+        workers.append(worker)
+    mp.Process(target = _overlord, args = (workers, results))
+    return workers
 
-    return len(lines) + 3
+@typechecked
+def _fill_tasks(folder_in: pathlib.Path, tasks: mp.Queue) -> None:
+    for file_name in folder_in.iterdir():
+        tasks.put(str(file_name))
+    tasks.put(_sentinel)
+
+@typechecked
+def _save_results(results: mp.Queue, jsonl_out: pathlib.Path) -> None:
+    bar_i = 0
+    widgets = [ 'Aggregating Document # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
+    with pb.ProgressBar(widgets = widgets) as bar:
+        with open(jsonl_out, 'w', encoding = 'utf-8') as fp:
+            with jl.Writer(fp, compact = True, sort_keys = True) as writer:
+                while True:
+                    bar.update(bar_i)
+                    bar_i = bar_i + 1
+                    json = results.get()
+                    if json == _sentinel:
+                        break
+                    else:                        
+                        writer.write(json)
+
+@typechecked
+def _process_tasks(tasks: mp.Queue, results: mp.Queue) -> None:
+    while True:
+        file_name = tasks.get()
+        if file_name == _sentinel:
+            tasks.put(_sentinel)
+            tasks.close()
+            break            
+        else:
+            file_name = pathlib.Path(file_name)
+            with open(file_name, 'r', encoding = 'utf-8') as fp:
+                lines = fp.readlines()
+            lines = [line.strip() for line in lines]
+            json = { 'file_name' : file_name.name, 'lines' : lines }
+            results.put(json)
+    results.close()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -64,11 +95,11 @@ if __name__ == '__main__':
         type = pathlib.Path,
         default = 'd:/corpus_in')
     parser.add_argument(
-        '-out', '--json-out',
-        help = 'JSON containing the aggregated corpus',
+        '-out', '--jsonl-out',
+        help = 'JSONL containing the aggregated corpus',
         type = pathlib.Path,
-        default = 'd:/corpus.json')
+        default = 'd:/corpus.jsonl')
     args = parser.parse_args()
     print(f'folder in: {args.folder_in}')
-    print(f'JSON out: {args.json_out}')
-    corpus_to_json(args.folder_in, args.json_out)
+    print(f'JSONL out: {args.jsonl_out}')
+    corpus_to_json(args.folder_in, args.jsonl_out)
