240179f61ff645a152d57d4f3d91f4b1e79b2540
Mark Newman <mark@trinetteandmark.com>
2021-02-14 22:11:42 -0500

    jsonl -> jsonl (smaller)

---
+++ code/prune_jsonl.py
@@ -0,0 +1,125 @@
+import csv
+import pathlib
+import json
+import jsonlines as jl
+import mp_boilerplate as mpb
+import typing as t
+import utils as u
+from argparse import ArgumentParser
+from typeguard import typechecked
+
+@typechecked
+def prune_jsonl(jsonl_in: pathlib.Path, jsonl_out: pathlib.Path, extract: t.List[str], sub_process_count: int) -> None:
+    """
+    Extract elments from a `JSONL` file making a _smaller_ `JSONL` file.
+
+    Parameters
+    ----------
+    jsonl_in : pathlib.Path
+        The JSONL containing all the documents
+    jsonl_out : pathlib.Path
+        The JSONL file containing all the documents
+    extract : List[str]
+        The name(s) of the elements to extract
+    sub_process_count : int
+        The number of sub processes used to transformation from in to out formats
+    """
+
+    if jsonl_out.exists():
+        jsonl_out.unlink()
+
+    worker = mpb.EPTS(
+        extract = _list_documents, extract_args = (jsonl_in),
+        transform = _extract_document, transform_init = _passthrough, transform_init_args = (extract),
+        save = _save_documents, save_args = (jsonl_out),
+        worker_count = sub_process_count,
+        show_progress = True)
+    worker.start()
+    worker.join()
+
+@typechecked
+def _list_documents(jsonl_in: pathlib.Path) -> t.Iterator[dict]:
+    """
+    Lists the documents in the `JSONL` file
+
+    Parameters
+    ----------
+    jsonl_in : pathlib.Path
+        The JSONL containing all the documents
+    """
+    encoding = u.guess_encoding(jsonl_in)
+    with open(jsonl_in, 'r', encoding = encoding) as fp:
+        with jl.Reader(fp) as reader:
+            for item in reader:
+                yield item
+
+@typechecked
+def _extract_document(state: t.List[str], document: dict) -> dict:
+    """
+    Extracts parts of the document 
+
+    Parameters
+    ----------
+    state : List[str]
+        The elements to extract
+    document : dict
+        The document to be saved
+    """
+    result = {}
+    for elm in state:
+        if elm in document:
+            result[elm] = document[elm]
+    return result
+
+@typechecked
+def _passthrough(extract: t.List[str]) -> t.List[str]:
+    """
+    Pass the state from the main thread to the single document processing function
+    """
+    return extract
+
+@typechecked
+def _save_documents(documents: t.Iterator[dict], jsonl_out: pathlib.Path) -> None:
+    """
+    Saves the documents to JSONL
+    
+    Parameters
+    ----------
+    documents : Iterator[dict]
+        The `dict`s to save
+    jsonl_out : pathlib.Path
+        The JSONL file containing all the documents
+    """
+    with open(jsonl_out, 'w', encoding = 'utf-16') as fp:
+        with jl.Writer(fp, compact = True, sort_keys = True) as writer:
+            for document in documents:
+                writer.write(document)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-in', '--jsonl-in',
+        help = 'The JSONL containing all the documents',
+        type = pathlib.Path,
+        required = True)
+    parser.add_argument(
+        '-out', '--jsonl-out',
+        help = 'The JSONL file containing all the documents',
+        type = pathlib.Path,
+        required = True)
+    parser.add_argument(
+        '-e', '--extract',
+        help = 'The name(s) of the elements to extract',
+        type = u.csv_list,
+        default = 'id')
+    parser.add_argument(
+        '-spc', '--sub-process-count',
+        help = 'The number of sub processes used to transformation from in to out formats',
+        type = int,
+        default = 1)
+    args = parser.parse_args()    
+    print(f'jsonl in: {args.jsonl_in}')
+    print(f'jsonl out: {args.jsonl_out}')
+    print(f'extract: {args.extract}')
+    print(f'sub process count: {args.sub_process_count}')
+    prune_jsonl(args.jsonl_in, args.jsonl_out, args.extract, args.sub_process_count)
---
+++ docs/prune_jsonl.md
@@ -0,0 +1,21 @@
+# JSONL to JSON
+
+Extract elments from a `JSONL` file making a _smaller_ `JSONL` file.
+
+# Steps
+
+Below are the steps needed to run the _extraction_ process.
+The pathing can be changed by updating the parameters.
+
+1. Clone this repository.
+2. Open a PowerShell window to the `~/code` directory.
+3. [Extract](../code/prune_jsonl.md) elments from a `JSONL` file making a _smaller_ `JSONL` file.
+   * The `-in`/`-out` parameters control the source and destination file.
+     If the output folder does not exist it is created.
+   * The `e` parameter is used to select the elements to extract.
+     It is a csv list.
+   * The optional `-spc` parameter allows for tuning on multi core machines.
+     It defaults to 1.
+   ```{ps1}
+   python prune_jsonl.py -in d:/corpus_in.jsonl -out d:/corpus_out.jsonl -e id,text
+   ```
